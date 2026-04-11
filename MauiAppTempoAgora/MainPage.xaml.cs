da85c20402e23a4a0959dbda7b84275ebac048a1
Kelly Rodrigues <kellycristina80@yahoo.com.br>
2026-04-11 17:01:32 -0300

    execuções da agenda 8 porem contem erros de sobreposição de respostas na tela de retorno

--- MauiAppTempoAgora/MainPage.xaml.cs
+++ MauiAppTempoAgora/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using MauiAppTempoAgora.Models;
 using MauiAppTempoAgora.Services;
+using System.Diagnostics;
 namespace MauiAppTempoAgora
 {
     public partial class MainPage : ContentPage
@@ -11,7 +12,7 @@ namespace MauiAppTempoAgora
             InitializeComponent();
         }
 
-        private async void Button_Clicked(object sender, EventArgs e)
+        private async void Button_Clicked_Previsão(object sender, EventArgs e)
         {
             try
             {
@@ -34,6 +35,15 @@ namespace MauiAppTempoAgora
                                          $"Visibilidade: {t.visibility} m \n";
 
                         lbl_res.Text = dados_previsao;
+                        string mapa = $"https://embed.windy.com/embed.html?" +
+                                     $"type=map&location=coordinates&metricRain=mm&metricTemp=°C" +
+                                     $"&metricWind=km/h&zoom=5&overlay=wind&product=ecmwf&level=surface" +
+                                     $"&lat={t.lat.ToString().Replace(",", ".")}&lon={t.lon.ToString().Replace(",", ".")}";
+
+                        wv_mapa.Source = mapa;
+
+                        Debug.WriteLine(mapa);
+
 
                     }
                     else
@@ -50,6 +60,67 @@ namespace MauiAppTempoAgora
             catch (Exception ex)
             {
                 await DisplayAlert("Ops", ex.Message, "OK");
+            }
+        }
+
+        private async void Button_Clicked_Localizacao(object sender, EventArgs e)
+        {
+
+            try
+            {
+                GeolocationRequest request = new GeolocationRequest(
+                    GeolocationAccuracy.Medium,
+                    TimeSpan.FromSeconds(10));
+                Location? local = await Geolocation.Default.GetLocationAsync(request);
+                if (local != null)
+                {
+                    string dados_localizacao = $"Latitude: {local.Latitude} \n" +
+                                              $"Longitude: {local.Longitude} \n" +
+                                              $"Altitude: {local.Altitude} m \n" +
+                                              $"Precisão: {local.Accuracy} m \n" +
+                                              $"Tempo da última atualização: {local.Timestamp} \n";
+                    lbl_res.Text = dados_localizacao;
+                }
+                else
+                {
+                    lbl_res.Text = "Não foi possível obter a localização.";
+                }
+
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                await DisplayAlert("Erro: Dispositivo não Suporta", fnsEx.Message, "OK");
+            }
+            catch (FeatureNotEnabledException fneEx)
+            {
+                await DisplayAlert("Erro: Localização Desabilitada", fneEx.Message, "OK");
+            }
+            catch (PermissionException pEx)
+            {
+                await DisplayAlert("Erro: Permissão da Localização", pEx.Message, "OK");
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Erro", ex.Message, "OK");
+            }
+
+        }
+        private async void GetCidade(double lat, double lon)
+        {
+            try
+            {
+                IEnumerable<Placemark> places = await Geocoding.Default.GetPlacemarksAsync(lat, lon);
+
+                Placemark? place = places.FirstOrDefault();
+
+                if (place != null)
+                {
+                    txt_cidade.Text = place.Locality;
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Erro: Obtenção do nome da Cidade", ex.Message, "OK");
             }
         }
     }
